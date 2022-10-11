1769644a611e703a0b060482b0e98ec59d6ec582
kaviraj <kavirajk36kv@gmail.com>
2022-10-11 15:22:03 +0530

    singlylinked list which with insertion and deletion

--- DS/linkedlist.py
+++ DS/linkedlist.py
@@ -1,82 +1,78 @@
-#Empty node creation
-class Node():
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-        
-#creating empty linked list
-class LinkedList():
-    def __init__(self):
-        self.head=None
-        
-    #traversing or printing linkedlist
-    def printll(self):
-        if self.head is None:
-            print('The linked list is empty')
-            return
-        else:
-            node=self.head
-            while node is not None:
-                print(node.data)
-                node=node.next
-    #inserting node
-    #inserting at begining
-    def insertfirst(self,node):
-        if self.head is None:
-            self.head=node
-        else:
-            node.next=self.head
-            self.head=node
-    #inserting at last
-    def insertatlast(self,node):
-        if self.head is None:
-            self.head=node
-        else:
-            for currnode in self:
-                pass
-            currnode.next=node
-    #inserting inbetween
-    #insert after a target node
-    def insert_after(self,target_node,newnode):
-        if self.head is None:
-            raise Exception('Linkedlist is empty')
-        for node in self:
-            if node.data==target_node:
-                newnode.next=node.next
-                node.next=newnode
-        raise Exception('Element not found')
-    
-    #insert before the target node
-    def insert_before(self,target_node,newnode):
-        if self.head is None:
-            raise Exception('Linkedlist is empty')
-        if self.head.data==target_node:
-            return self.insertfirst(newnode)
-        prev_node=self.head
-        for node in self:
-            if node.data==target_node:
-                prev_node.next=newnode
-                newnode.next=node
-                return
-            prev_node=node
-        raise Exception('Element not found')
-    #deleting an element
-    def remove(self,target):
-        if self.head is None:
-            raise Exception('Linkedlist is empty')
-        if self.head==target:
-            self.head.next=self.head
-            return
-        prev_node=self.head
-        for node in self:
-            if node.data==target:
-                prev_node.next=node.next
-                return
-            prev_node=node
-        raise Exception('Element not found')
-ll=LinkedList()
-ll.insertfirst(Node(6))
-#ll.insertfirst(Node(7))
-#ll.remove(6)
-ll.printll()
+#initaializaton of node for a singly linked list
+class Node:
+  def __init__(self,data):
+    self.data=data
+    self.next=None
 
+#class for a initiation of singlylinked list
+class SingleLL:
+  def __init__(self):
+    self.head=None
+  #printsll function can be used to traverse the list
+  def printsll(self):
+    if self.head==None:
+      print('list is empty')
+    else:
+      n=self.head
+      while n is not None:
+        print(n.data,end='-->')
+        n=n.next
+        
+  def insert_at_first(self,data):
+    newnode=Node(data)
+    if self.head==None:
+      self.head=newnode
+    else:
+      newnode.next=self.head
+      self.head=newnode
+  
+  def insert_at_last(self,data):
+    newnode=Node(data)
+    if self.head==None:
+      self.head=newnode
+    else:
+      n=self.head
+      while n.next is not None:
+        n=n.next
+      n.next=newnode
+
+  def insertat(self,position,data):
+    newnode=Node(data)
+    if position==0:
+      return self.insert_at_first(data)
+    n=self.head
+    for i in range(1,position):
+      n=n.next
+    newnode.next=n.next
+    n.next=newnode
+
+  def removenode(self,key):
+    if self.head==None:
+        print('list is empty')
+    prev=self.head
+    a=self.head.next
+    if prev.data==key:
+      self.head=a
+      a=None
+    else:
+      while a is not None:
+        if a.data==key:
+          prev.next=a.next
+        prev=prev.next
+        a=a.next
+     
+if __name__=='__main__':
+    sll=SingleLL()
+    sll.insert_at_first(6)
+    sll.insert_at_first(5)
+    sll.insert_at_last(7)
+    sll.insert_at_last(10)
+    sll.insertat(3,8)
+    sll.insertat(4,9)
+    sll.insertat(0,4)
+    sll.printsll()
+    sll.removenode(10)
+    sll.removenode(4)
+    sll.removenode(7)
+    print()
+    sll.printsll()
