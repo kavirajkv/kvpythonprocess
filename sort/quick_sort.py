9fd4cb5ac28baced5922e94d23928791b9272206
kaviraj <kavirajk36kv@gmail.com>
2022-10-12 17:29:40 +0530

    updated quick sort

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,29 +1,19 @@
-def quick(L,start,end):
-    pivot=L[0]
-    start=pivot+1
-    end=len(L)-1
+def quicksort(myl):
+    elem=len(myl)
+    if elem<2:
+        return myl
+    current=0
     
-    while start<=end:
-        while start<=end and L[start]<=pivot:
-            start=start+1
-        while start<=end and L[end]>=pivot:
-            end=end-1
-            
-        if start<end:
-            L[start],L[end]=L[end],L[start]
-            
-    pivot,L[start]=L[start],pivot
-    return start
+    for i in range(1,elem):
+        if myl[i]<=myl[0]:
+            current=current+1
+            myl[i],myl[current]=myl[current],myl[i]
+    myl[0],myl[current]=myl[current],myl[0]
+    quicksort(myl[0:current])
+    quicksort(myl[current+1:elem])
+    return myl
 
-def quicksort(Li,start,end):
-    if start<end:
-        q=quick(Li,start,end)
-        quick(Li,start,q-1)
-        quick(Li,q+1,end)
-
-
-
-Li=[3,7,8,13,2,5]
-qu=quicksort(Li,0,len(Li)-1)
-print(qu)
-    
+if __name__=='__main__': 
+    mylist=[3,2,7,9,1,6]
+    mysort=quicksort(mylist)
+    print(mysort)
